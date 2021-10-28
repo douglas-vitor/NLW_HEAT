f2e41f7c2b83dd02a9e6fedda250f095bf55b624
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-10-28 19:58:43 -0300

    Stage3

--- NLW-APP/src/hooks/auth.tsx
+++ NLW-APP/src/hooks/auth.tsx
@@ -61,6 +61,7 @@ function AuthProvider({ children }: AuthProviderProps) {
                 api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
                 await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user));
                 await AsyncStorage.setItem(TOKEN_STORAGE, token);
+                
                 setUser(user);
             }
         } catch (err) {
@@ -71,6 +72,9 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
 
     async function signOut() {
+        setUser(null);
+        await AsyncStorage.removeItem(USER_STORAGE);
+        await AsyncStorage.removeItem(TOKEN_STORAGE);
     }
 
     useEffect(() => {
